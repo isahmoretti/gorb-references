a7aa58d8fb4d3f2076311193112cff48bd976417
Matheus Paice <matheus.paice@ikatec.com.br>
2020-07-07 18:36:55 -0300

    add formik

--- src/views/book/index.js
+++ src/views/book/index.js
@@ -1,37 +1,55 @@
-import React from 'react';
-import { useFormik, Form } from "formik";
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  author: Yup.string().required("Required"),
+});
+
 function Book() {
-    // Elementos essenciais: autor, título, subtítulo (se houver),
-    // edição (se houver), local de publicação, editora e data de publicação.
-    // Elementos complementares: outras responsabilidades, paginação, série, notas, ISBN.
-    const formik = useFormik({
-        initialValues: {
-            author: '',
-            title: '',
-            caption: '',  // subtitulo - não é obrigatório
-            edition: '', // não é obrigatório
-            local: '',  // ex: São Paulo
-            publishingCompany: '', //
-            yearOfPublication: '',
-            online: false,
-        }
-    })
-    return (
-        <div>
-            Livros
-            <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="author">Author</label>
-                <input
-                    id="author"
-                    name="author"
-                    type="author"
-                    onChange={formik.handleChange}
-                    value={formik.values.author}
-                />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
+  // Elementos essenciais: autor, título, subtítulo (se houver),
+  // edição (se houver), local de publicação, editora e data de publicação.
+  // Elementos complementares: outras responsabilidades, paginação, série, notas, ISBN.
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      Livros
+      <Formik
+        initialValues={{
+          author: "matheus paice",
+          title: "",
+          caption: "", // subtitulo - não é obrigatório
+          edition: "", // não é obrigatório
+          local: "", // ex: São Paulo
+          publishingCompany: "", //
+          yearOfPublication: "",
+          online: false,
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={handleSubmit}
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit}>
+            <input
+              type="text"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.author}
+              name="author"
+            />
+            {props.errors.author && (
+              <span className="error">{props.errors.author}</span>
+            )}
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default Book;
