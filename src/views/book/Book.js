2cf95824c013105fb15b61d6fe241f3998ac89ce
Daniel Barbosa de Lima <daniel.lima@ikatec.com.br>
2020-07-07 19:37:34 -0300

    a

--- src/views/book/Book.js
+++ src/views/book/Book.js
@@ -2,8 +2,15 @@ import React from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
+import InputWrapper from "../../components/InputWrapper/Input";
+
+
 const SignupSchema = Yup.object().shape({
-  author: Yup.string().required("Required"),
+  author: Yup.string().required("Obrigatório"),
+  title: Yup.string().required("Obrigatório"),
+  local: Yup.string().required("Obrigatório"),
+  publishingCompany: Yup.string().required("Obrigatório"),
+  yearOfPublication: Yup.string().required("Obrigatório"),
 });
 
 const Book = () => {
@@ -12,15 +19,15 @@ const Book = () => {
   // Elementos complementares: outras responsabilidades, paginação, série, notas, ISBN.
 
   const handleSubmit = (values) => {
+
     console.log(values);
   };
 
   return (
     <div>
-      Livros
       <Formik
         initialValues={{
-          author: "matheus paice",
+          author: "",
           title: "",
           caption: "", // subtitulo - não é obrigatório
           edition: "", // não é obrigatório
@@ -34,8 +41,9 @@ const Book = () => {
       >
         {(props) => (
           <form onSubmit={props.handleSubmit}>
-            <input
+            <InputWrapper
               type="text"
+              label="Autor"
               onChange={props.handleChange}
               onBlur={props.handleBlur}
               value={props.values.author}
@@ -44,6 +52,79 @@ const Book = () => {
             {props.errors.author && (
               <span className="error">{props.errors.author}</span>
             )}
+            <InputWrapper
+              type="text"
+              label="Título"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.title}
+              name="title"
+            />
+            {props.errors.title && (
+              <span className="error">{props.errors.title}</span>
+            )}
+            <InputWrapper
+              type="text"
+              label="Capítulo"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.caption}
+              name="caption"
+            />
+            <InputWrapper
+              type="text"
+              label="Edição"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.edition}
+              name="edition"
+            />
+            <InputWrapper
+              type="text"
+              label="Local de publicação"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.local}
+              name="local"
+            />
+            {props.errors.local && (
+              <span className="error">{props.errors.local}</span>
+            )}
+            <InputWrapper
+              type="text"
+              label="Empresa de publicação"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.publishingCompany}
+              name="publishingCompany"
+            />
+            {props.errors.publishingCompany && (
+              <span className="error">{props.errors.publishingCompany}</span>
+            )}
+            <InputWrapper
+              type="text"
+              label="Ano de publicação"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.yearOfPublication}
+              name="yearOfPublication"
+            />
+            {props.errors.yearOfPublication && (
+              <span className="error">{props.errors.yearOfPublication}</span>
+            )}
+            <label htmlFor="online">Online</label>
+            <select
+              id="online"
+              label="online"
+              value={props.values.online}
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              name="online"
+            >
+              <option value="true">Sim</option>
+              <option value="false">Não</option>
+            </select>
+
             <button type="submit">Submit</button>
           </form>
         )}
@@ -53,3 +134,5 @@ const Book = () => {
 };
 
 export default Book;
+
+
