019a0d482d7f3d834d8e224ebbf28d76d4939e71
Daniel Barbosa de Lima <daniel.lima@ikatec.com.br>
2020-07-07 18:26:48 -0300

    a

--- src/views/book/index.js
+++ src/views/book/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import { useFormik, Form } from "formik";
 function Book() {
-  return (
-    <div>
-      Livros
-    </div>
-  );
+    // Elementos essenciais: autor, título, subtítulo (se houver),
+    // edição (se houver), local de publicação, editora e data de publicação.
+    // Elementos complementares: outras responsabilidades, paginação, série, notas, ISBN.
+    const formik = useFormik({
+        initialValues: {
+            author: '',
+            title: '',
+            caption: '',  // subtitulo - não é obrigatório
+            edition: '', // não é obrigatório
+            local: '',  // ex: São Paulo
+            publishingCompany: '', //
+            yearOfPublication: '',
+            online: false,
+        }
+    })
+    return (
+        <div>
+            Livros
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="author">Author</label>
+                <input
+                    id="author"
+                    name="author"
+                    type="author"
+                    onChange={formik.handleChange}
+                    value={formik.values.author}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default Book;
