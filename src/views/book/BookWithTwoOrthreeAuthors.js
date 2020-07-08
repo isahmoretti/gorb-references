a305f22ae3198bbff696207bd2a98fe899e23255
Daniel Barbosa de Lima <daniel.lima@ikatec.com.br>
2020-07-07 22:38:59 -0300

    adiciona novas coisas

---
+++ src/views/book/BookWithTwoOrthreeAuthors.js
@@ -0,0 +1,218 @@
+import React, { useState } from "react";
+
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+import { Grid } from "@material-ui/core";
+
+// components
+import Input from "../../components/InputWrapper/Input";
+import Button from "../../components/Buttons";
+
+// styles
+import { Container, Card, Row, Content, Footer } from "./style";
+
+const SignupSchema = Yup.object().shape({
+  author: Yup.string().required("Obrigatório"),
+  title: Yup.string().required("Obrigatório"),
+  local: Yup.string().required("Obrigatório"),
+  publishingCompany: Yup.string().required("Obrigatório"),
+  yearOfPublication: Yup.string().required("Obrigatório"),
+});
+
+const generateReference = (values) => {
+  const {
+    author,
+    title,
+    caption,
+    edition,
+    local,
+    publishingCompany,
+    yearOfPublication,
+  } = values;
+
+  const authorSplit = author.split(" ");
+
+  const firstName = authorSplit[0];
+
+  const lastName = authorSplit[authorSplit.length - 1].toUpperCase(); // TODO: ultimo sobrenome ou primeiro?
+
+  return (
+    <span>
+      {" "}
+      {lastName}, {firstName}. {' '}
+      {caption ? (
+        <>
+          <b>{title}: </b>
+          {caption}.{" "}
+        </>
+      ) : (
+          <b>{title}. </b>
+        )}
+      {edition && <> {edition}. ed. </>}
+      {local}: {publishingCompany}, {' '}
+      {yearOfPublication}.
+    </span>
+  );
+};
+const Book = () => {
+  const [state, setState] = useState({
+    values: {},
+  });
+
+  const handleSubmit = (values) => {
+    setState((prev) => ({
+      ...prev,
+      values,
+      references: generateReference(values),
+    }));
+  };
+
+  return (
+    <Container>
+      <Formik
+        initialValues={{
+          author: "Daniel Barbosa de Lima",
+          title: "A bela e a fera",
+          caption: "Em uma aventura perigosa", // subtitulo - não é obrigatório
+          edition: "1", // não é obrigatório
+          local: "São Paulo", // ex: São Paulo
+          publishingCompany: "Revista hoje", //
+          yearOfPublication: "2020",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={handleSubmit}
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit}>
+            <Card>
+              <Content>
+                <Grid container spacing={2} style={{ marginBottom: 30 }}>
+                  <Grid item xs={4}>
+                    <Input
+                      type="text"
+                      label="Autor"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.author}
+                      name="author"
+                      errors={props.errors}
+                      touched={props.touched}
+                    />
+                  </Grid>
+
+                  <Grid item xs={4}>
+                    <Input
+                      type="text"
+                      label="Título"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.title}
+                      name="title"
+                      errors={props.errors}
+                      touched={props.touched}
+                    />
+                  </Grid>
+
+                  <Grid item xs={4}>
+                    <Input
+                      type="text"
+                      label="Subtítulo"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.caption}
+                      name="caption"
+                      errors={props.errors}
+                      touched={props.touched}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid container spacing={2} style={{ marginBottom: 30 }}>
+                  <Grid item xs={4}>
+                    <Input
+                      type="text"
+                      label="Edição"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.edition}
+                      name="edition"
+                      errors={props.errors}
+                      touched={props.touched}
+                    />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Input
+                      type="text"
+                      label="Local de publicação"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.local}
+                      name="local"
+                      errors={props.errors}
+                      touched={props.touched}
+                    />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Input
+                      type="text"
+                      label="Empresa de publicação"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.publishingCompany}
+                      name="publishingCompany"
+                      errors={props.errors}
+                      touched={props.touched}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid container spacing={2} style={{ marginBottom: 30 }}>
+                  <Grid item xs={3}>
+                    <Input
+                      type="text"
+                      label="Ano de publicação"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.yearOfPublication}
+                      name="yearOfPublication"
+                      errors={props.errors}
+                      touched={props.touched}
+                    />
+                  </Grid>
+
+                  {/* <Grid item xs={6}>
+                    <Select
+                      id="online"
+                      label="Online"
+                      value={props.values.online}
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      name="online"
+                      options={[
+                        { value: true, name: "Sim" },
+                        { value: false, name: "Não" },
+                      ]}
+                    />
+                  </Grid> */}
+                </Grid>
+              </Content>
+
+              {state.references && generateReference(state.values)}
+              <Footer>
+                <Row container className="end">
+                  <Button variant="outlined" color="primary">
+                    Limpar campos
+                  </Button>
+                  <Button type="submit" color="primary">
+                    Gerar referencia
+                  </Button>
+                </Row>
+              </Footer>
+            </Card>
+          </form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
+
+export default Book;
