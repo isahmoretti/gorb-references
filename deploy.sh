3d6351bc7ab5bbd3bf21b331af7b357d47a3aa16
Matheus Paice <matheus.paice@ikatec.com.br>
2020-07-08 13:29:56 -0300

    add script deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git pull
+
+yarn install
+
+# rm -rf dist/
+
+# echo "dist folder removed";
+
+# yarn build
+
+# node --max_old_space_size=512 node_modules/.bin/webpack --mode=production
