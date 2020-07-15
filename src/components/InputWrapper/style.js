d78e5226ba5ca64eb5263cb813085bbb93037179
Matheus Paice <matheus.paice@ikatec.com.br>
2020-07-15 20:02:35 -0300

    fix:add help

--- src/components/InputWrapper/style.js
+++ src/components/InputWrapper/style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { HelpOutline } from "styled-icons/material";
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -12,4 +14,26 @@ export const ErrorText = styled.span`
   font-size: 12px;
 
   color: red;
+`;
+
+export const IconHelp = styled(HelpOutline)`
+  width: 24px;
+  height: 24px;
+
+  color: #6666cc;
+`;
+
+export const ButtonHelp = styled.button`
+  background: transparent;
+
+  outline: none;
+  border: none;
+`;
+
+export const ContainerHelp = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  width: 100%;
 `;
--- src/views/book/BookWithOneAuthor.js
+++ src/views/book/BookWithOneAuthor.js
@@ -234,7 +234,7 @@ const Book = ({ back }) => {
                     />
                   </Grid>
                 </Grid>
-                <Grid container spacing={2} style={{ marginBottom: 30 }}>
+                <Grid container spacing={2} style={{ marginBottom: 15 }}>
                   <Grid item xs={12} sm={12} md={4}>
                     <Input
                       disabled={!props.values.complementaryElements}
@@ -288,6 +288,8 @@ const Book = ({ back }) => {
                       name="isbn"
                       errors={props.errors}
                       touched={props.touched}
+                      help
+                      helpText="Número de livro padrão internacional"
                     />
                   </Grid>
                 </Grid>
