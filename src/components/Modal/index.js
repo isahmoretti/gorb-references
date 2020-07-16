ff940efff262a6e05ae49978d5b80cb04c026b36
Matheus Paice <matheus.paice@ikatec.com.br>
2020-07-15 21:15:49 -0300

    fix:width modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
@@ -7,17 +7,27 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 
 function SimpleDialog(props) {
   const { isOpen, handleClose, text } = props;
+  const [copy, setCopy] = useState(false);
+
+  useEffect(() => {
+    const copyText = document.getElementById("myInput")?.textContent;
+
+    console.log("copyText: ", copyText);
+  }, [copy]);
 
   return (
     <Dialog
       onClose={handleClose}
       aria-labelledby="simple-dialog-title"
       open={isOpen}
+      fullWidth
+      maxWidth="md"
     >
       <DialogTitle id="simple-dialog-title">Referência</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          {text}
+          <div id="myInput">{text}</div>
+          <button onClick={() => setCopy(!copy)}> Copy </button>
         </DialogContentText>
       </DialogContent>
     </Dialog>
