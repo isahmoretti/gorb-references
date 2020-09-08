2d015f97866adc130dbccb412a7653a20203d352
Matheus Paice <matheus.paice@ikatec.com.br>
2020-09-08 03:12:20 -0300

    fix(url):adiciona url de producao

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:3333",
+  baseURL: "http://api.paice-io.com",
   headers: {
     "Content-Type": "application/json",
   },
