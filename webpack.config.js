a729ee959b3b5aa06aad91afeffbad3da5e78599
Matheus Paice <matheus.paice@ikatec.com.br>
2020-09-23 08:52:40 -0300

    fix:build

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+// const dotenv = require('dotenv');
+
+module.exports = () => {
+  // const env = dotenv.config().parsed;
+
+  // const envKeys = Object.keys(env).reduce((prev, next) => {
+  //     prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  //     return prev;
+  // }, {});
+
+  return {
+    // mode: process.env.NODE_ENV,
+    devtool: "source-map",
+    entry: ["@babel/polyfill", "./src/index.js"],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/",
+    },
+    devServer: {
+      contentBase: "./dist",
+      historyApiFallback: true,
+      port: 4000,
+      compress: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|j?g|svg|gif)?$/,
+          use: "file-loader",
+        },
+      ],
+    },
+    plugins: [
+      // new webpack.DefinePlugin(envKeys),
+      new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, "public/index.html"),
+        filename: "index.html",
+      }),
+      //   new CopyWebpackPlugin([
+      //     { from: "src/assets/favicons", to: "favicons" },
+      //     { from: "src/assets/public", to: "assets" },
+      //   ]),
+    ],
+  };
+};
