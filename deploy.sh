849cfb423f6cf659f98b54cf414ad8f4f9d0ef0d
Matheus Paice <matheus.paice@ikatec.com.br>
2020-09-27 23:45:20 -0300

    fix:corrige build da aplicacao que o dani quebrou

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ git pull
 
 yarn install
 
-# rm -rf dist/
+rm -rf dist/
 
-# echo "dist folder removed";
+echo "dist folder removed";
+
+node --max_old_space_size=512 node_modules/.bin/webpack --mode=production
 
 # yarn build
-
-# node --max_old_space_size=512 node_modules/.bin/webpack --mode=production
