e0e0f93e137f4ea12e4a58ba1f577307da2fa2f9
Matheus Paice <matheus.paice@ikatec.com.br>
2020-11-28 06:50:06 -0300

    fix:adiciona routas do gorb

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -84,43 +84,121 @@ const AppRoutes = () => {
   return (
     <Router>
       <Switch>
-        {/* <Route exact path="/" component={Hexagon} />
-        
+        <Route exact path="/" component={Hexagon} />
+
         <Route exact path="/livro" component={Book} />
         <Route exact path="/livro/um-autor" component={BookWithOneAuthor} />
-        <Route exact path="/livro/dois-ou-tres-autores" component={BookWithTwoOrThreeAuthors} />
-        <Route exact path="/livro/quatro-autores-ou-mais" component={BookWithFourOrMoreAuthors} />
-        <Route exact path="/livro/responsavel-intelectual" component={BookIntellectuallyResponsible} />
-        <Route exact path="/livro/autor-entidade" component={BookAuthorEntity} />
-        <Route exact path="/livro/capitulo-de-livro" component={ChapterOfBook} />
+        <Route
+          exact
+          path="/livro/dois-ou-tres-autores"
+          component={BookWithTwoOrThreeAuthors}
+        />
+        <Route
+          exact
+          path="/livro/quatro-autores-ou-mais"
+          component={BookWithFourOrMoreAuthors}
+        />
+        <Route
+          exact
+          path="/livro/responsavel-intelectual"
+          component={BookIntellectuallyResponsible}
+        />
+        <Route
+          exact
+          path="/livro/autor-entidade"
+          component={BookAuthorEntity}
+        />
+        <Route
+          exact
+          path="/livro/capitulo-de-livro"
+          component={ChapterOfBook}
+        />
 
-        
         <Route exact path="/trabalhos-academicos" component={AcademicWork} />
-        <Route exact path="/trabalhos-academicos/artigo-de-periodico" component={WorkArticlePeriodic} />
-        <Route exact path="/trabalhos-academicos/artigo-de-revista" component={ArticleMagazine} />
-        <Route exact path="/trabalhos-academicos/artigo-de-jornal" component={ArticleNewspaper} />
-        <Route exact path="/trabalhos-academicos/dissertacao" component={Dissertation} />
-        <Route exact path="/trabalhos-academicos/monografia" component={Monography} />
+        <Route
+          exact
+          path="/trabalhos-academicos/artigo-de-periodico"
+          component={WorkArticlePeriodic}
+        />
+        <Route
+          exact
+          path="/trabalhos-academicos/artigo-de-revista"
+          component={ArticleMagazine}
+        />
+        <Route
+          exact
+          path="/trabalhos-academicos/artigo-de-jornal"
+          component={ArticleNewspaper}
+        />
+        <Route
+          exact
+          path="/trabalhos-academicos/dissertacao"
+          component={Dissertation}
+        />
+        <Route
+          exact
+          path="/trabalhos-academicos/monografia"
+          component={Monography}
+        />
         <Route exact path="/trabalhos-academicos/teses" component={Thesis} />
 
-        
-        <Route exact path="/meio-eletronico" component={ElectronicMediaDocuments} />
-        <Route exact path="/meio-eletronico/artigo-de-blog" component={BlogArticle} />
+        <Route
+          exact
+          path="/meio-eletronico"
+          component={ElectronicMediaDocuments}
+        />
+        <Route
+          exact
+          path="/meio-eletronico/artigo-de-blog"
+          component={BlogArticle}
+        />
         <Route exact path="/meio-eletronico/ebook" component={Ebook} />
         <Route exact path="/meio-eletronico/email" component={Email} />
-        <Route exact path="/meio-eletronico/mensagens-instantaneas" component={InstantMessages} />
+        <Route
+          exact
+          path="/meio-eletronico/mensagens-instantaneas"
+          component={InstantMessages}
+        />
         <Route exact path="/meio-eletronico/site" component={Site} />
-        <Route exact path="/meio-eletronico/apresentacao-de-slide" component={SlideShow} />
-        <Route exact path="/meio-eletronico/postagem-na-rede-social" component={SocialNetworkPost} />
-        <Route exact path="/meio-eletronico/programa-e-jogo-eletronico" component={SoftwareAndEletronicGame} />
+        <Route
+          exact
+          path="/meio-eletronico/apresentacao-de-slide"
+          component={SlideShow}
+        />
+        <Route
+          exact
+          path="/meio-eletronico/postagem-na-rede-social"
+          component={SocialNetworkPost}
+        />
+        <Route
+          exact
+          path="/meio-eletronico/programa-e-jogo-eletronico"
+          component={SoftwareAndEletronicGame}
+        />
 
         <Route exact path="/evento" component={Events} />
-        <Route exact path="/evento/revistas" component={EventsWorkPublishedInMagazines} />
+        <Route
+          exact
+          path="/evento/revistas"
+          component={EventsWorkPublishedInMagazines}
+        />
         <Route exact path="/evento/patentes" component={Patents} />
-        <Route exact path="/evento/normas-tecnicas" component={TechnicalStandards} />
+        <Route
+          exact
+          path="/evento/normas-tecnicas"
+          component={TechnicalStandards}
+        />
         <Route exact path="/evento/todo" component={WholeEvent} />
-        <Route exact path="/evento/todo-em-periodica" component={WholeEventInPeriodicPublication} />
-        <Route exact path="/evento/trabalhos-em-anais" component={WorksInAnnals} />
+        <Route
+          exact
+          path="/evento/todo-em-periodica"
+          component={WholeEventInPeriodicPublication}
+        />
+        <Route
+          exact
+          path="/evento/trabalhos-em-anais"
+          component={WorksInAnnals}
+        />
 
         <Route exact path="/audiovisual" component={Audiovisual} />
         <Route exact path="/audiovisual/audio-livro" component={AudioBook} />
@@ -128,31 +206,63 @@ const AppRoutes = () => {
         <Route exact path="/audiovisual/musica" component={Music} />
         <Route exact path="/audiovisual/foto" component={Photo} />
         <Route exact path="/audiovisual/podcast" component={Podcast} />
-        <Route exact path="/audiovisual/video-internet" component={VideoInternet} />
-        
+        <Route
+          exact
+          path="/audiovisual/video-internet"
+          component={VideoInternet}
+        />
+
         <Route exact path="/outros" component={Other} />
         <Route exact path="/outros/bula-de-remedio" component={BullMedicine} />
         <Route exact path="/outros/verbete" component={Entry} />
         <Route exact path="/outros/mapas" component={Maps} />
-        <Route exact path="/outros/objetos-tridimensional" component={ObjectThree} />
+        <Route
+          exact
+          path="/outros/objetos-tridimensional"
+          component={ObjectThree}
+        />
         <Route exact path="/outros/partitura" component={Score} />
         <Route exact path="/outros/obra-de-arte" component={WorkArt} />
-        
-        <Route exact path="/documentos-juridicos" component={JuridicDocument} />
-        <Route exact path="/documentos-juridicos/atos-administrativos" component={AdministrativeActs} />
-        <Route exact path="/documentos-juridicos/documentos-civil" component={CivilAndNotary} />
-        <Route exact path="/documentos-juridicos/constituicao" component={Constitution} />
-        <Route exact path="/documentos-juridicos/jurisprudencia" component={Jurisprudence} />
-        <Route exact path="/documentos-juridicos/legislacao" component={Legislation} />
-        <Route exact path="/documentos-juridicos/medida-provisoria" component={ProvisionalMeasure} /> */}
 
-        {/* <Route path="*">
+        <Route exact path="/documentos-juridicos" component={JuridicDocument} />
+        <Route
+          exact
+          path="/documentos-juridicos/atos-administrativos"
+          component={AdministrativeActs}
+        />
+        <Route
+          exact
+          path="/documentos-juridicos/documentos-civil"
+          component={CivilAndNotary}
+        />
+        <Route
+          exact
+          path="/documentos-juridicos/constituicao"
+          component={Constitution}
+        />
+        <Route
+          exact
+          path="/documentos-juridicos/jurisprudencia"
+          component={Jurisprudence}
+        />
+        <Route
+          exact
+          path="/documentos-juridicos/legislacao"
+          component={Legislation}
+        />
+        <Route
+          exact
+          path="/documentos-juridicos/medida-provisoria"
+          component={ProvisionalMeasure}
+        />
+
+        <Route path="*">
           <Redirect to="/" />
-        </Route> */}
+        </Route>
 
         {/* teste vocacional */}
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/test" component={VocationalTest} />
+        {/* <Route exact path="/" component={HomePage} />
+        <Route exact path="/test" component={VocationalTest} /> */}
       </Switch>
     </Router>
   );
